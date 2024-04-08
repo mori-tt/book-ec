ba1e16d69cef180f38f36512ccf5f31b9695766b
T_Mori <morit0301@gmail.com>
2024-04-08 22:03:16 +0900

    🚧 購入履歴の保存の作業中

---
+++ src/app/api/checkout/success/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../lib/prisma";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export async function POST(request: Request, response: Response) {
+  const { sessionId } = await request.json();
+
+  try {
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+    const existingPurchase = await prisma.purchase.findFirst({
+      where: {
+        userId: session.client_reference_id!,
+        bookId: session.metadata?.bookId!,
+      },
+    });
+
+    if (!existingPurchase) {
+      const purchase = await prisma.purchase.create({
+        data: {
+          userId: session.client_reference_id!,
+          bookId: session.metadata?.bookId!,
+        },
+      });
+      return NextResponse.json({ purchase });
+    } else {
+      return NextResponse.json({ message: "すでに購入済みです。" });
+    }
+  } catch (err) {
+    return NextResponse.json(err);
+  }
+}
