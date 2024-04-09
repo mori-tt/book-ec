5d42d7f1749afeb067aa739806b41960e5e25559
T_Mori <morit0301@gmail.com>
2024-04-10 05:25:36 +0900

    💄 記事詳細、購入履歴の追加

--- src/app/book/checkout-success/page.tsx
+++ src/app/book/checkout-success/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 const PurchaseSuccess = () => {
+  const [bookUrl, setBookUrl] = useState(null);
   const searchParams = useSearchParams();
   const sessionId = searchParams.get("session_id");
 
@@ -11,7 +12,7 @@ const PurchaseSuccess = () => {
     const fetchData = async () => {
       if (sessionId) {
         try {
-          console.log(sessionId);
+          // console.log(sessionId);
           const res = await fetch(
             `${process.env.NEXT_PUBLIC_API_URL}/checkout/success`,
             {
@@ -20,7 +21,9 @@ const PurchaseSuccess = () => {
               body: JSON.stringify({ sessionId }),
             }
           );
-          console.log(await res.json());
+          // console.log(await res.json());
+          const data = await res.json();
+          setBookUrl(data.purchase.bookId);
         } catch (err) {
           console.error(err);
         }
@@ -40,7 +43,7 @@ const PurchaseSuccess = () => {
         </p>
         <div className="mt-6 text-center">
           <Link
-            href={`/`}
+            href={`/book/${bookUrl}`}
             className="text-indigo-600 hover:text-indigo-800 transition duration-300"
           >
             購入した記事を読む
--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -12,3 +12,11 @@ export const getAllBooks = async () => {
   });
   return allBooks;
 };
+
+export const getDetailBook = async (contentId: string) => {
+  const detailBook = await client.getListDetail<BookType>({
+    endpoint: "book-ec",
+    contentId,
+  });
+  return detailBook;
+};
