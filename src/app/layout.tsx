a64b5977870b226b1f63d6a5ad645cb0059e33fa
T_Mori <morit0301@gmail.com>
2024-04-11 01:42:12 +0900

    完成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import { NextAuthProvider } from "./lib/next-auth/provider";
 import { Suspense } from "react";
+
 import Loading from "./loading";
 
 const notoSansJP = Noto_Sans_JP({ subsets: ["latin"], weight: "400" });
