19ba3ad3822b7ceb49c7ca1a4d629e6abef1835c
T_Mori <morit0301@gmail.com>
2024-04-11 06:13:26 +0900

    ⚡️ page.tsxで取得したuserをBookコンポーネントのプロップスで渡す

--- src/app/components/Book.tsx
+++ src/app/components/Book.tsx
@@ -1,21 +1,21 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
-import { BookType } from "../types/types";
+import { BookType, User } from "../types/types";
 import React, { useState } from "react";
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 type BookProps = {
   book: BookType;
   isPurchased: boolean;
+  user: User;
 };
 
 // eslint-disable-next-line react/display-name
-const Book = ({ book, isPurchased }: BookProps) => {
+const Book = ({ book, isPurchased, user }: BookProps) => {
   const [showModal, setShowModal] = useState(false);
-  const { data: session } = useSession();
-  const user: any = session?.user;
+  // const { data: session } = useSession();
+  // const user: any = session?.user;
   const router = useRouter();
 
   const startCheckout = async () => {
